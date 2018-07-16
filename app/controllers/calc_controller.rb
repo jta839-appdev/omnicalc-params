74f133a9fdbc983a9db6b426b8a9079201ff143b
Tom Alford <jalford0@chicagobooth.edu>
2018-07-16 19:15:43 +0000

    Work

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -9,4 +9,50 @@ class CalcController < ApplicationController
         render("calc_templates/flexible_square.html.erb")
         
     end
+    
+    def flex_square_root
+        #The parameters look like this:  {"the_num" => "5"}
+        #Rails puts stuff in params
+        
+        @num_input = params.fetch("the_num").to_i
+        @num_output = @num_input**0.5
+        
+        render("calc_templates/flexible_square_root.html.erb")
+        
+    end
+    
+    def flex_payment
+        #The parameters look like this:  {"the_num" => "5"}
+        #Rails puts stuff in params
+        
+        @basis_points = params.fetch("basis_points").to_i
+        @num_years = params.fetch("num_years").to_i
+        @principal = params.fetch("principal").to_i
+        
+        @rate = (@basis_points/100.0)
+        @payment = ((@rate/100.0)/12.0*@principal)/(1-(1+(@rate/100.0)/12.0)**(-@num_years*12.0))
+        
+        @rate = sprintf('%.2f', @rate)
+        @payment = sprintf('%.2f', @payment)
+        
+        render("calc_templates/flexible_payment.html.erb")
+        
+    end
+    
+    def flex_random
+        #The parameters look like this:  {"the_num" => "5"}
+        #Rails puts stuff in params
+        
+        @min_input = params.fetch("min_input").to_i
+        @max_input = params.fetch("max_input").to_i
+        
+        range = @max_input - @min_input
+        @num_output = @min_input + rand(range + 1)
+        
+        
+        render("calc_templates/flexible_random.html.erb")
+        
+    end
+   
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     
     get("flexible/square/:the_num", { :controller => "calc" , :action => "flex_square"})
-    
+    get("flexible/square_root/:the_num", { :controller => "calc" , :action => "flex_square_root"})
+    get("flexible/payment/:basis_points/:num_years/:principal", { :controller => "calc" , :action => "flex_payment"})
+    get("flexible/random/:min_input/:max_input", { :controller => "calc" , :action => "flex_random"})
 
 end
